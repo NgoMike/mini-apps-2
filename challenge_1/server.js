88b3006fd0498506c88991f0a6ea1b8af14a1a46
Michael <mqngo21@gmail.com>
2018-04-04 11:54:30 -0700

    additional setup and templates

--- challenge_1/server.js
+++ challenge_1/server.js
@@ -1,2 +1,13 @@
+const express = require('express');
+const path = require('path');
+const bodyParser = require('body-parser');
 
+const app = express();
 
+app.use(bodyParser.json());
+app.use(express.static(path.join(__dirname, './public')));
+
+const port = 8000;
+app.listen(port, () => console.log(`Server is up on: ${port}`));
+
+module.exports = app;
--- challenge_1/webpack.config.js
+++ challenge_1/webpack.config.js
@@ -1,1 +1,19 @@
+const path = require('path');
 
+module.exports = {
+  entry: path.resolve(__dirname, './client/app.jsx'),
+  output: {
+    path: path.resolve(__dirname, './public'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        use: {
+          loaders: ['eslint-loader', 'babel-loader'],
+        },
+      },
+    ],
+  },
+};
